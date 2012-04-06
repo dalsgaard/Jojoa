fd14bcd5ed09d8f52b9761371249eb900bf2ea91
Kim Dalsgaard <kim@kimdalsgaard.com>
2012-04-06 14:02:00 +0200

    Misc

--- static/demo.js
+++ static/demo.js
@@ -10,83 +10,30 @@ function setup() {
   var bundle = new Bundle(views);
 
   var mainViewController = bundle.viewControllers.main;
-  pageController.presentViewController(mainViewController);
+  pageController.presentViewController(mainViewController, false);
 
+  var navController = bundle.viewControllers.nav;
   var fooViewController = bundle.viewControllers.foo;
+  var barViewController = bundle.viewControllers.bar;
+
+  navController.pushViewController(barViewController);
+
+  var pushFoo = barViewController.view.querySelector("button.push-foo");
+  pushFoo.addEventListener('click', function(e) {
+    navController.pushViewController(fooViewController, true);
+    e.stopPropagation();
+  }, false);  
+
   var dismissFoo = fooViewController.view.querySelector("button.dismiss-foo");
   dismissFoo.addEventListener('click', function(e) {
-    fooViewController.pageController.dismissViewController();
+    pageController.dismissViewController(true);
     e.stopPropagation();
   }, false);
 
   var presentFoo = mainViewController.view.querySelector("button.present-foo");
   presentFoo.addEventListener('click', function(e) {
-    pageController.presentViewController(fooViewController);
+    pageController.presentViewController(navController, true);
     e.stopPropagation();
   }, false);
-
-  /*
-  var mainPage = document.querySelector("section.view.main-page");
-  var fooPage = document.querySelector("section.view.foo-page");
-  var bazPage = document.querySelector("section.view.baz-page");
-  var fooNav = fooPage.querySelector("section.navigation");
-
-  mainPage.addEventListener('click', clickMain, false);
-  fooPage.addEventListener('click', clickFoo, false);
-  fooNav.addEventListener('click', clickFooNav, false);
-  bazPage.addEventListener('click', clickBaz, false);
-
-  page.appendChild(mainPage);
-
-  function clickMain(e) {
-    presentView(fooPage);
-  }
-
-  function clickFoo(e) {
-    dismissView(fooPage);
-  }
-  
-  function clickFooNav(e) {
-    pushView(fooNav, bazPage);
-    e.stopPropagation();
-  }
-
-  function clickBaz(e) {
-    popView(fooNav, bazPage);
-    e.stopPropagation();
-  }
-
-  function pushView(navigation, view) {
-    navigation.classList.add('push');
-    navigation.querySelector("section.container").appendChild(view);
-    window.setTimeout(function() {
-      navigation.classList.remove('push');
-    }, 1000);
-  }
-
-  function popView(navigation, view) {
-    navigation.classList.add('pop');
-    window.setTimeout(function() {
-      views.appendChild(view);
-      navigation.classList.remove('pop');
-    }, 2000);
-  }
-
-  function presentView(view) {
-    view.classList.add('modal');
-    view.classList.add('appear');
-    page.appendChild(view);
-  }
-
-  function dismissView(view) {
-    view.classList.remove('appear');
-    view.classList.add('disappear');
-    window.setTimeout(function() {
-      views.appendChild(view);
-      view.classList.remove('disappear');
-      view.classList.remove('modal');
-    }, 1000);
-  }
-  */
 
 };
--- static/jojoa.js
+++ static/jojoa.js
@@ -3,43 +3,166 @@ function PageController(page) {
   this.page = page;
   this.viewControllers = [];
 }
-
 PageController.prototype.presentViewController = presentViewController;
 PageController.prototype.dismissViewController = dismissViewController;
 
-function presentViewController(viewController) {
-  viewController.view.classList.add('modal');
-  viewController.view.classList.add('appear');
+function presentViewController(viewController, animated) {
   this.page.appendChild(viewController.view);
   this.viewControllers.push(viewController);
   viewController.pageController = this;
+  if (animated) {
+    viewController.view.classList.add('appear');
+  } else {
+    if (viewController.viewWillAppear) {
+      viewController.viewWillAppear(false);
+    }
+    if (viewController.viewDidAppear) {
+      viewController.viewDidAppear(false);
+    }
+  }
 }
 
-function dismissViewController() {
-  var viewController = this.viewControllers.pop();
-  viewController.view.classList.remove('appear');
-  viewController.view.classList.add('disappear');
-  window.setTimeout(function() {
-    this.page.removeChild(viewController.view);
-    viewController.view.classList.remove('disappear');
-    viewController.view.classList.remove('modal');
-    viewController.pageController = null;
-  }, 1000);
+function dismissViewController(animated) {
+  var controller = this.viewControllers.pop();
+  if (animated) {
+    controller.view.classList.add('disappear');
+  } else {
+    if (controller.viewWillDisappear) {
+      controller.viewWillDisappear(false);
+    }
+    controller.pageController.page.removeChild(controller.view);
+    controller.pageController = null;
+    if (controller.viewDidDisappear) {
+      controller.viewDidDisappear(false);
+    }
+  }
 }
 
 
-function ViewController(view) {
+function AbstractViewController() { }
+AbstractViewController.prototype.init = function(view) {
   this.view = view;
   this.pageController = null;
+  var t = this;
+  this.view.addEventListener('webkitAnimationStart', function(e) {
+    if (e.animationName === "modal-appear") {
+      if (t.viewWillAppear) t.viewWillAppear(true);
+    } else {
+      if (t.viewWillDisappear) t.viewWillDisappear(true);
+    }
+    e.stopPropagation();
+  }, false);
+  this.view.addEventListener('webkitAnimationEnd', function(e) {
+    if (e.animationName === "modal-appear") {
+      this.classList.remove('appear');
+      if (t.viewDidAppear) t.viewDidAppear(true);
+    } else {
+      t.pageController.page.removeChild(this);
+      this.classList.remove('disappear');
+      t.pageController = null;
+      if (t.viewDidDisappear) t.viewDidDisappear(true);
+    }
+    e.stopPropagation();
+  }, false);
 }
+
+function ViewController(view) {
+  this.init(view);
+}
+ViewController.prototype = new AbstractViewController();
+
+
+function NavigationController(view) {
+  this.init(view);
+  this.container = view.querySelector("section.container");
+  this.back = view.querySelector("header > nav.back > *");
+  var t = this;
+  this.back.addEventListener('click', function(e) {
+    t.popViewController(true);
+  }, false);
+  this.viewControllers = [];
+  var t = this;
+  this.container.addEventListener('webkitAnimationStart', function(e) {
+    var controller = t.viewControllers[t.viewControllers.length - 1];
+    if (e.animationName === "navigation-push") {
+      if (controller.viewWillAppear) controller.viewWillAppear(true);
+    } else {
+      if (controller.viewWillDisappear) controller.viewWillDisappear(true);
+    }
+    e.stopPropagation();
+  }, false);
+  this.container.addEventListener('webkitAnimationEnd', function(e) {
+    var controller;
+    if (e.animationName === "navigation-push") {
+      controller = t.viewControllers[t.viewControllers.length - 1];
+      this.classList.remove('push');
+      if (controller.viewDidAppear) controller.viewDidAppear(true);
+    } else {
+      controller = t.viewControllers.pop();
+      this.removeChild(controller.view);
+      this.classList.remove('pop');
+      controller.navigationController = null;
+      if (controller.viewDidDisappear) controller.viewDidDisappear(true);
+    }
+    e.stopPropagation();
+  }, false);
+}
+NavigationController.prototype = new AbstractViewController();
+NavigationController.prototype.pushViewController = pushViewController;
+NavigationController.prototype.popViewController = popViewController;
+
+function pushViewController(viewController, animated) {
+  this.container.appendChild(viewController.view);
+  this.viewControllers.push(viewController);
+  viewController.navigationController = this;
+  if (animated) {
+    this.container.classList.add('push');
+  } else {
+    if (viewController.viewWillAppear) {
+      viewController.viewWillAppear(false);
+    }
+    if (viewController.viewDidAppear) {
+      viewController.viewDidAppear(false);
+    }
+  }
+}
+
+function popViewController(animated) {
+  if (this.viewControllers.length > 1) {
+    if (animated) {
+      this.container.classList.add('pop');
+    } else {
+      var controller = this.viewControllers.pop();
+      if (controller.viewWillDisappear) {
+        controller.viewWillDisappear(false);
+      }
+      this.container.removeChild(controller.view);
+      controller.navigationController = null;
+      if (controller.viewDidDisappear) {
+        controller.viewDidDisappear(false);
+      }
+    }
+  }
+}
+
+
 
 function Bundle(bundle) {
   this.viewControllers = {};
-  var views = bundle.querySelectorAll("section.view");
+  var views = bundle.querySelectorAll("section.view:not(.navigation)");
   for (var i = 0; i < views.length; i++) {
     var view = views[i];
     var controller = new ViewController(view);
     var name = view.getAttribute('data-name');
+    this.viewControllers[name] = controller;
+  }
+  this.navigationControllers = {};
+  var navs = bundle.querySelectorAll("section.view.navigation");
+  for (var i = 0; i < navs.length; i++) {
+    var nav = navs[i];
+    var controller = new NavigationController(nav);
+    var name = nav.getAttribute('data-name');
+    this.navigationControllers[name] = controller;
     this.viewControllers[name] = controller;
   }
 }
